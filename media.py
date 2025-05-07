be4b663cad1edd7362cb652c864f0e56254ac4e0
brunoat12 <34278792+brunoat12@users.noreply.github.com>
2025-05-06 22:16:03 -0300

    Versao na FUNCA

---
+++ media.py
@@ -0,0 +1,11 @@
+#ALTERACAO DE ARQUIVO PARA EXERCICIO
+#Fazendo uma alteração na master
+titulo = "Media de dois valores"
+print(f'{titulo:^30}')
+
+x = float(input('Entre com o valor de x: '))
+y = float(input('Entre com o valor de y: '))
+media = (x+y)/2
+print("O valor de x é", x, "!")
+print('O valor de y é', y)
+print('A media dos valores é', round(media, 3))
